b6ae5df7e4fb1dea7d6fb27412b82a54e81c6718
einar.formica <einar.formica@106-07.info2>
2025-10-20 10:13:20 +0200

    lunes

---
+++ src/ejerciciosswitch/Switch4.java
@@ -0,0 +1,70 @@
+package ejerciciosswitch;
+
+import java.util.Scanner;
+
+public class Switch4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String primeraTirada;
+		String segundaTirada;
+		int valorNumPrimeraTirada;
+		int valorNumSegundaTirada;
+		int valorNumTotal;
+		
+		Scanner teclado= new Scanner(System.in);
+		
+		System.out.println("¿Cuánto has sacado en tu primera tirada?");
+		primeraTirada = teclado.next();
+		switch(primeraTirada){
+		case "Uno":
+			valorNumPrimeraTirada=1;
+			break;
+		case "Dos":
+			valorNumPrimeraTirada=2;
+			break;
+		case "Tres":
+			valorNumPrimeraTirada=3;
+			break;
+		case "Cuatro" :
+			valorNumPrimeraTirada=4;
+			break;
+		case "Cinco":
+			valorNumPrimeraTirada=5;
+			break;
+		case "Seis" :
+			valorNumPrimeraTirada=6;
+			break;}
+		
+		
+		
+		System.out.println("¿Cuánto has sacado en tu segunda tirada?");
+		switch(primeraTirada){
+		case "Uno":
+			valorNumSegundaTirada=1;
+			break;
+		case "Dos":
+			valorNumSegundaTirada=2;
+			break;
+		case "Tres":
+			valorNumSegundaTirada=3;
+			break;
+		case "Cuatro" :
+			valorNumSegundaTirada=4;
+			break;
+		case "Cinco":
+			valorNumSegundaTirada=5;
+			break;
+		case "Seis" :
+			valorNumSegundaTirada=6;
+			break;
+			valorNumTotal = valorNumPrimeraTirada + valorNumSegundaTirada;
+			System.out.println("La suma de las dos tiradas es " + valorNumTotal);
+			}
+		
+		
+		
+		
+	}
+
+}
--- src/tema2/ifelse/Ud2ejercicio2.java
+++ src/tema2/ifelse/Ud2ejercicio2.java
@@ -8,21 +8,16 @@ public class Ud2ejercicio2 {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
 		double número;
-		
+
 		System.out.println("Introduzca un numero");
 		número = teclado.nextDouble();
-		
-		
-		if (número > -1 && número != 0 && número <1)
-			{
+
+		if (número > -1 && número != 0 && número < 1) {
 			System.out.println("Su número  es casi cero");
-			}
-		else 
-			{
+		} else {
 			System.out.println("Su número no es casi cero");
-			}
+		}
 		teclado.close();
-		
 
 	}
 
