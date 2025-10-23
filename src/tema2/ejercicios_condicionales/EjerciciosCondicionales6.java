833cbb0737a3c0f4d5bf39139590625bf47e506e
einarformica-code <einar.formica@iesnervion.es>
2025-10-23 14:30:50 +0200

    commitjueves

---
+++ src/tema2/ejercicios_condicionales/EjerciciosCondicionales6.java
@@ -0,0 +1,30 @@
+package tema2.ejercicios_condicionales;
+
+import java.util.Scanner;
+
+public class EjerciciosCondicionales6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		int año;
+		int mes;
+		int diasMes;
+		
+		
+		
+		System.out.println("Dime que año es");
+		año = teclado.nextInt();
+		boolean bisiesto = año % 4== 0;
+		
+		System.out.println("Mes");
+		mes = teclado.nextInt();
+		
+		if (bisiesto && mes==2) {
+			diasMes
+		
+	}
+
+}
